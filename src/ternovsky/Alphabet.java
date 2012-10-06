dd1f65f9fad21ae57d6d8b7eff62b560fa167cce
ternovsky <aleksey.ternovsky@gmail.com>
2012-10-06 23:30:47 +0400

    First commit

---
+++ src/ternovsky/Alphabet.java
@@ -0,0 +1,12 @@
+package ternovsky;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ternovsky
+ * Date: 06.10.12
+ * Time: 23:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Alphabet {
+
+}
